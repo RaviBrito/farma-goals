5d4c99a98325c1d4373ed6158c07ab992fb67259
Ravi Brito <ravibriton@gmail.com>
2024-04-22 10:48:33 -0300

    Construido CRUD de categorias

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,8 +7,8 @@ import './Home.css';
 function Home() {
     return (
         <>
-        <div className="bg-indigo-900 flex justify-center">
-          <div className='container grid grid-cols-2 text-white'>
+        <div className="bg-white flex justify-center">
+          <div className='container grid grid-cols-2 text-slate-900'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
               <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
               <p className='text-xl'>Farma Goals</p>
